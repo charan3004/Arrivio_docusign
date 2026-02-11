7bb63a73fae6e837a55e77cbc1693773ef396c71
likhithakaruparti <likhithakaruparti@gmail.com>
2026-02-11 22:26:04 +0530

    robo UI fully done

--- src/components/common/ArrivioRobot.jsx
+++ src/components/common/ArrivioRobot.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import "./ArrivioRobot.css";
 import neutralRobot from "../../assets/robot/robo-hi.png";
 
 export default function ArrivioRobot() {
-  return (
-    <div className="robot-wrapper">
-      {/* Speech Bubble */}
-      <div className="robot-bubble">
-        Hey there, I can assist you :)
-      </div>
+  const [visible, setVisible] = useState(false);
+  const [showBubble, setShowBubble] = useState(false);
 
-      {/* Floating Robot */}
+  const triggerBubble = () => {
+    setShowBubble(true);
+    setVisible(true);
+
+    setTimeout(() => {
+      setVisible(false); // start fade out
+      setTimeout(() => {
+        setShowBubble(false); // remove from DOM after fade
+      }, 300); // match CSS fade duration
+    }, 4000);
+  };
+
+  useEffect(() => {
+    triggerBubble();
+  }, []);
+
+  return (
+    <div className="robot-wrapper" onMouseEnter={triggerBubble}>
+      {showBubble && (
+        <div className={`robot-bubble ${visible ? "fade-in" : "fade-out"}`}>
+          Hey there, I can assist you :)
+        </div>
+      )}
+
       <div className="robot-inner">
         <img
           src={neutralRobot}
